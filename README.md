50dbd7f5215338dd4aa306ec47c1a1b081b74279
kali <kali@kali.kali>
2026-03-11 02:37:00 -0400

    First Port Scanner Tool

---
+++ README.md
@@ -0,0 +1,16 @@
+# Port Scanner Tool
+
+Simple Python port scanner for cybersecurity learning.
+
+## Features
+- Scan common ports
+- Detect open ports
+- Fast scanning
+
+## Usage
+
+python3 port_scanner.py
+
+## Example
+
+Enter target IP or domain: scanme.nmap.org
---
+++ port_scanner.py
@@ -0,0 +1,19 @@
+import socket
+
+target = input("Enter target IP or domain: ")
+
+ports = [21,22,23,25,53,80,110,139,143,443,445,3389]
+
+print("Scanning target:", target)
+print("-----------------------------")
+
+for port in ports:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+
+    result = s.connect_ex((target, port))
+
+    if result == 0:
+        print(f"Port {port} is OPEN")
+
+    s.close()
